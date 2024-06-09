a96e3a2ce7eabf2fe0e101c6e136319ad6c55eaa
Medjit Saitov <saitovmedjit@gmail.com>
2024-06-09 20:34:42 +0300

    NetowrkManager set connection to sta and ap mode

--- main/NetworkManager/NetworkManager.h
+++ main/NetworkManager/NetworkManager.h
@@ -5,6 +5,13 @@
 #include "freertos/task.h"
 #include "esp_log.h"
 
+#include <string.h>
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "nvs_flash.h"
+
 //Tag used for logging
 static const char *TAG_NETWORK_MANAGER = "NETWORK_MANAGER";
 
@@ -12,11 +19,164 @@ static const char *TAG_NETWORK_MANAGER = "NETWORK_MANAGER";
 void networkManager_init(void);
 static void networkManager_task(void *pvParameter);
 
+//================================================================
+
+#define CONFIG_STA_CONNECT_TIMEOUT  10000
+#define CONFIG_STA_WIFI_SSID        "test_wifi"
+#define CONFIG_STA_WIFI_PASSWORD    "123456789"
+#define CONFIG_AP_WIFI_SSID         "PPP"
+#define CONFIG_AP_WIFI_PASSWORD     ""
+#define CONFIG_AP_WIFI_CHANNEL      1
+#define CONFIG_AP_MAX_STA_CONN      5
+
+static EventGroupHandle_t wifi_event_group;
+const int CONNECTED_BIT = BIT0;
+
+static void event_handler(void* arg, esp_event_base_t event_base,
+								int32_t event_id, void* event_data)
+{
+	if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+		ESP_LOGI(TAG_NETWORK_MANAGER, "WIFI_EVENT_STA_DISCONNECTED");
+		esp_wifi_connect();
+		xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
+	} else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+		ESP_LOGI(TAG_NETWORK_MANAGER, "IP_EVENT_STA_GOT_IP");
+		xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
+	}
+}
+
+static void initialise_wifi(void)
+{
+	esp_log_level_set("wifi", ESP_LOG_WARN);
+	static bool initialized = false;
+	if (initialized) {
+		return;
+	}
+	ESP_ERROR_CHECK(esp_netif_init());
+	wifi_event_group = xEventGroupCreate();
+	ESP_ERROR_CHECK(esp_event_loop_create_default());
+	esp_netif_t *ap_netif = esp_netif_create_default_wifi_ap();
+	assert(ap_netif);
+	esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
+	assert(sta_netif);
+	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+	ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+	ESP_ERROR_CHECK( esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &event_handler, NULL) );
+	ESP_ERROR_CHECK( esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL) );
+
+	ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+	ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_NULL) );
+	ESP_ERROR_CHECK( esp_wifi_start() );
+
+	initialized = true;
+}
+
+static bool wifi_ap(void)
+{
+	wifi_config_t wifi_config = { 0 };
+	strcpy((char *)wifi_config.ap.ssid,CONFIG_AP_WIFI_SSID);
+	strcpy((char *)wifi_config.ap.password, CONFIG_AP_WIFI_PASSWORD);
+	wifi_config.ap.authmode = WIFI_AUTH_WPA_WPA2_PSK;
+	wifi_config.ap.ssid_len = strlen(CONFIG_AP_WIFI_SSID);
+	wifi_config.ap.max_connection = CONFIG_AP_MAX_STA_CONN;
+	wifi_config.ap.channel = CONFIG_AP_WIFI_CHANNEL;
+
+	if (strlen(CONFIG_AP_WIFI_PASSWORD) == 0) {
+		wifi_config.ap.authmode = WIFI_AUTH_OPEN;
+	}
+
+
+	ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_AP) );
+	ESP_ERROR_CHECK( esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config) );
+	ESP_ERROR_CHECK( esp_wifi_start() );
+	ESP_LOGI(TAG_NETWORK_MANAGER, "WIFI_MODE_AP started. SSID:%s password:%s channel:%d",
+			 CONFIG_AP_WIFI_SSID, CONFIG_AP_WIFI_PASSWORD, CONFIG_AP_WIFI_CHANNEL);
+	return ESP_OK;
+}
+
+static bool wifi_sta(int timeout_ms)
+{
+	wifi_config_t wifi_config = { 0 };
+	strcpy((char *)wifi_config.sta.ssid, CONFIG_STA_WIFI_SSID);
+	strcpy((char *)wifi_config.sta.password, CONFIG_STA_WIFI_PASSWORD);
+
+	ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
+	ESP_ERROR_CHECK( esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
+	ESP_ERROR_CHECK( esp_wifi_connect() );
+
+	int bits = xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT,
+								   pdFALSE, pdTRUE, timeout_ms / portTICK_PERIOD_MS);
+	ESP_LOGI(TAG_NETWORK_MANAGER, "bits=%x", bits);
+	if (bits) {
+		ESP_LOGI(TAG_NETWORK_MANAGER, "WIFI_MODE_STA connected. SSID:%s password:%s",
+			 CONFIG_STA_WIFI_SSID, CONFIG_STA_WIFI_PASSWORD);
+	} else {
+		ESP_LOGI(TAG_NETWORK_MANAGER, "WIFI_MODE_STA can't connected. SSID:%s password:%s",
+			 CONFIG_STA_WIFI_SSID, CONFIG_STA_WIFI_PASSWORD);
+	}
+	return (bits & CONNECTED_BIT) != 0;
+}
+
+static bool wifi_apsta(int timeout_ms)
+{
+	wifi_config_t ap_config = { 0 };
+	strcpy((char *)ap_config.ap.ssid,CONFIG_AP_WIFI_SSID);
+	strcpy((char *)ap_config.ap.password, CONFIG_AP_WIFI_PASSWORD);
+	ap_config.ap.authmode = WIFI_AUTH_WPA_WPA2_PSK;
+	ap_config.ap.ssid_len = strlen(CONFIG_AP_WIFI_SSID);
+	ap_config.ap.max_connection = CONFIG_AP_MAX_STA_CONN;
+	ap_config.ap.channel = CONFIG_AP_WIFI_CHANNEL;
+
+	if (strlen(CONFIG_AP_WIFI_PASSWORD) == 0) {
+		ap_config.ap.authmode = WIFI_AUTH_OPEN;
+	}
+
+	wifi_config_t sta_config = { 0 };
+	strcpy((char *)sta_config.sta.ssid, CONFIG_STA_WIFI_SSID);
+	strcpy((char *)sta_config.sta.password, CONFIG_STA_WIFI_PASSWORD);
+
+
+	ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_APSTA) );
+	ESP_ERROR_CHECK( esp_wifi_set_config(ESP_IF_WIFI_AP, &ap_config) );
+	ESP_ERROR_CHECK( esp_wifi_set_config(ESP_IF_WIFI_STA, &sta_config) );
+	ESP_ERROR_CHECK( esp_wifi_start() );
+	ESP_LOGI(TAG_NETWORK_MANAGER, "WIFI_MODE_AP started. SSID:%s password:%s channel:%d",
+			 CONFIG_AP_WIFI_SSID, CONFIG_AP_WIFI_PASSWORD, CONFIG_AP_WIFI_CHANNEL);
+
+	ESP_ERROR_CHECK( esp_wifi_connect() );
+	int bits = xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT,
+								   pdFALSE, pdTRUE, timeout_ms / portTICK_PERIOD_MS);
+	ESP_LOGI(TAG_NETWORK_MANAGER, "bits=%x", bits);
+	if (bits) {
+		ESP_LOGI(TAG_NETWORK_MANAGER, "WIFI_MODE_STA connected. SSID:%s password:%s",
+			 CONFIG_STA_WIFI_SSID, CONFIG_STA_WIFI_PASSWORD);
+	} else {
+		ESP_LOGI(TAG_NETWORK_MANAGER, "WIFI_MODE_STA can't connected. SSID:%s password:%s",
+			 CONFIG_STA_WIFI_SSID, CONFIG_STA_WIFI_PASSWORD);
+	}
+	return (bits & CONNECTED_BIT) != 0;
+}
+
+
+//----------------------------------------------------------------
 
 //Functions
 static void networkManager_task(void *pvParameter)
 {
     ESP_LOGI(TAG_NETWORK_MANAGER, "Task created and running.");
+
+    esp_err_t err = nvs_flash_init();
+	if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+		ESP_ERROR_CHECK( nvs_flash_erase() );
+		err = nvs_flash_init();
+	}
+	ESP_ERROR_CHECK(err);
+
+	initialise_wifi();
+
+    //wifi_ap();
+    //wifi_sta(CONFIG_STA_CONNECT_TIMEOUT*1000);
+    wifi_apsta(CONFIG_STA_CONNECT_TIMEOUT*1000);
     while(true)
     {
         vTaskDelay(1000 / portTICK_PERIOD_MS);
